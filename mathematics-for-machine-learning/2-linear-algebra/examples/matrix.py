523f43b6fabeb97fa14cb4c9749ea5d217d2f213
zaindev81 <zaindev81@gmail.com>
2025-08-10 05:44:31 +0700

    examples

--- mathematics-for-machine-learning/2-linear-algebra/examples/matrix.py
+++ mathematics-for-machine-learning/2-linear-algebra/examples/matrix.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def normal_matrix():
+def basic_matrix():
     # Feature matrix (height and weight for 3 people)
     # 3 x 2
     # X => [[170, 65],
@@ -65,9 +65,10 @@ def transpose_matrix():
     # X.T @ w => [[170, 180, 160],
     #              [ 65,  75,  50]] @ [3, -2, 5]
 
-    # 1 row 170×3+180×(−2)+160×5=510−360+800=950
-    # 2 row 65×3+75×(−2)+50×5=195−150+250=295
+    # 1 row 170×3 + 180×(−2) + 160×5 = 510−360+800=950
+    # 2 row 65×3 + 75×(−2) + 50×5 = 195−150+250=295
 
-normal_matrix()
-print("=" * 30)
-transpose_matrix()
+if __name__ == "__main__":
+    basic_matrix()
+    print("=" * 30)
+    transpose_matrix()
--- mathematics-for-machine-learning/2-linear-algebra/examples/vector.py
+++ mathematics-for-machine-learning/2-linear-algebra/examples/vector.py
@@ -1,34 +1,58 @@
 import numpy as np
 
-v1 = np.array([1, 2, 3])
-v2 = np.array([4, 5, 6])
 
-# vector
-print("v1=", v1)
-print("v2=", v2)
+def basic_operation():
+    v1 = np.array([1, 2, 3])
+    v2 = np.array([4, 5, 6])
 
-# add
-print("v1 + v2 =", v1 + v2)
+    # vector
+    print("v1=", v1)
+    print("v2=", v2)
 
-# subtract
-print("v1 - v2 =", v1 - v2)
+    # add
+    print("v1 + v2 =", v1 + v2)
 
-# scalar multiplication
-print("2 * v1 =", 2 * v1)
+    # subtract
+    print("v1 - v2 =", v1 - v2)
 
-# dot product
-print("v1 . v2 =", np.dot(v1, v2))
+    # scalar multiplication
+    print("2 * v1 =", 2 * v1)
 
-# norm
-print("||v1|| =", np.linalg.norm(v1))
-print("||v2|| =", np.linalg.norm(v2))
+    # dot product
+    print("v1 . v2 =", np.dot(v1, v2))
 
-# height, weight
-person = np.array([170, 65])
-print("person =", person)
+    # norm
+    print("||v1|| =", np.linalg.norm(v1)) # 1, 4, 9
+    print("||v2|| =", np.linalg.norm(v2))
 
-# normalization
-length = np.linalg.norm(person)
-print("length of person =", length)
-normalized = person / length
-print("normalized person =", normalized)
+    # height, weight
+    person = np.array([170, 65])
+    print("person =", person)
+
+    # normalization
+    length = np.linalg.norm(person)
+    print("length of person =", length)
+    normalized = person / length
+    print("normalized person =", normalized)
+
+
+def vector_operations():
+    v1=  np.array([2, -1, 2])
+    v2 = np.array([-1, 4, 0])
+
+    print("add", v1 + v2)
+    print("subtract", v1 - v2)
+    print("scalar 2 * v", 2 * v1)
+    print("dot", v1.dot(v2)) # -2, -4, 0
+    print("norm1",np.linalg.norm(v1))
+    print("norm2",np.linalg.norm(v2))
+
+    person = np.array([180,60])
+    length = np.linalg.norm(person)
+    print("normalize", person / length)
+
+
+if __name__ == "__main__":
+    basic_operation()
+    print("=" * 30)
+    vector_operations()
